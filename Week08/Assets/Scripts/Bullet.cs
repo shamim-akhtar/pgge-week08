47349353a5caa5d01fc1dbf68ef3e8a7cc108904
Shamim Akhtar <shamim.akhtar@gmail.com>
2022-12-04 11:50:06 +0800

    Added darkcity scene.

---
+++ Week08/Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
